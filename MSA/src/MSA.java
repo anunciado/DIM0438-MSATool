fe58a92520ec9c58d442937ff6b7ffb6ce029dc5
Shirley Ohara Telémaco <31323126+Telemaco98@users.noreply.github.com>
2018-05-15 08:46:02 -0300

    Documenting MSA.java

--- MSA/src/MSA.java
+++ MSA/src/MSA.java
@@ -9,14 +9,29 @@ import org.biojava.nbio.core.sequence.compound.AminoAcidCompound;
 import org.biojava.nbio.core.sequence.io.FastaReaderHelper; 
 import org.biojava.nbio.core.util.ConcurrencyTools;
 
+/**
+ * @class   The MSA class has the aim to do the align of the proteins.
+ *
+ * @author  Luis Eduardo  (cruxiu@ufrn.edu.br)
+ * @author  Shirley Ohara (shirleyohara@ufrn.edu.br)
+ * @version 11.05.2018
+ */
 public class MSA {
 	
-   private ArrayList<String> ids;
+   private ArrayList<String> ids; /** < The id proteins */
 	
-   MSA(ArrayList<String> ids) {
+   /**
+    * The parameterized constructor
+    * @param ids - The proteins id
+    */
+   public MSA(ArrayList<String> ids) {
 	   this.ids = ids;
    }
 
+   /**
+    * This method returns a mutiple sequence alignment
+    * @return profile
+    */
    public String multipleSequenceAlignment() throws Exception {  
        List<ProteinSequence> lst = new ArrayList<ProteinSequence>();  
        for (String id : ids) {  
@@ -27,12 +42,14 @@ public class MSA {
        return profile.toString();
    }
 
+   /**
+    * This method receive a protein id and returns a protein sequence
+    * @return seq - The sequence
+    */
    public ProteinSequence getSequenceForId(String uniProtId) throws Exception {  
        URL uniprotFasta = new URL(String.format("http://www.uniprot.org/uniprot/%s.fasta", uniProtId));  
        ProteinSequence seq = FastaReaderHelper.readFastaProteinSequence(uniprotFasta.openStream()).get(uniProtId);  
        //System.out.printf("id : %s %s%n%s%n", uniProtId, seq, seq.getOriginalHeader());  
        return seq;  
    }
-
 }
-
